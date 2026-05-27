dcd1ba0d1be831736c442eb9f872278f2f2726e0
Rory Zac Cawley <rory.cawley@gmail.com>
2026-05-27 18:05:36 +0100

    US1 AC1: add todo with valid title returns pending

--- components/todo/src/com/rory/todo/interface.clj
+++ components/todo/src/com/rory/todo/interface.clj
@@ -1,1 +1,5 @@
 (ns com.rory.todo.interface)
+
+(defn add-todo [title]
+  {:title title
+   :status :pending})
--- components/todo/test/com/rory/todo/interface_test.clj
+++ components/todo/test/com/rory/todo/interface_test.clj
@@ -2,5 +2,8 @@
   (:require [clojure.test :as test :refer :all]
             [com.rory.todo.interface :as todo]))
 
-(deftest dummy-test
-  (is (= 1 1)))
+(deftest add-todo-with-valid-title
+  (testing "Given a valid title, when I add it, it appears as pending"
+    (let [result (todo/add-todo "Buy milk")]
+      (is (= "Buy milk" (:title result)))
+      (is (= :pending (:status result))))))
